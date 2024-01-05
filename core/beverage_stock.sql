99d5f1b2ec9b8749985706ccc347858ed280ca5e
DESKTOP-HFV34KO\Vinícius <vinicius.silva.grilo@gmail.com>
2024-01-05 19:31:30 -0300

    Updated database

--- core/beverage_stock.sql
+++ core/beverage_stock.sql
@@ -5,28 +5,7 @@
 -- Dumped from database version 16.1
 -- Dumped by pg_dump version 16.1
 
--- Started on 2024-01-04 14:03:43
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 4792 (class 1262 OID 16400)
--- Name: beverage_stock; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE beverage_stock WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'English_United States.1252';
-
-
-ALTER DATABASE beverage_stock OWNER TO postgres;
+-- Started on 2024-01-05 19:29:04
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -44,12 +23,12 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 215 (class 1259 OID 16401)
+-- TOC entry 216 (class 1259 OID 16465)
 -- Name: beverage; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.beverage (
-    id SERIAL,
+    id integer NOT NULL,
     name character varying(30) NOT NULL,
     price double precision NOT NULL,
     picture character varying(200) NOT NULL,
@@ -62,12 +41,37 @@ CREATE TABLE public.beverage (
 ALTER TABLE public.beverage OWNER TO postgres;
 
 --
--- TOC entry 216 (class 1259 OID 16406)
+-- TOC entry 215 (class 1259 OID 16464)
+-- Name: beverage_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.beverage_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.beverage_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4794 (class 0 OID 0)
+-- Dependencies: 215
+-- Name: beverage_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.beverage_id_seq OWNED BY public.beverage.id;
+
+
+--
+-- TOC entry 218 (class 1259 OID 16471)
 -- Name: user; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public."user" (
-    id SERIAL,
+    id integer NOT NULL,
     username character varying(15) NOT NULL,
     password character varying(20) NOT NULL,
     is_active boolean NOT NULL,
@@ -78,28 +82,48 @@ CREATE TABLE public."user" (
 ALTER TABLE public."user" OWNER TO postgres;
 
 --
--- TOC entry 4785 (class 0 OID 16401)
--- Dependencies: 215
--- Data for Name: beverage; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 217 (class 1259 OID 16470)
+-- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.beverage VALUES (DEFAULT,'Conhaque Presidente', 10, '-', 9, 'Destilado', true);
-INSERT INTO public.beverage VALUES (DEFAULT,'Skol', 2.99, '-', 86, 'Cerveja', true);
-INSERT INTO public.beverage VALUES (DEFAULT,'Tial Maracujá', 5.99, '-', 54, 'Suco', true);
+CREATE SEQUENCE public.user_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.user_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4795 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 
 
 --
--- TOC entry 4786 (class 0 OID 16406)
--- Dependencies: 216
--- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
+-- TOC entry 4639 (class 2604 OID 16468)
+-- Name: beverage id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-INSERT INTO public."user" VALUES (DEFAULT,'grilo', '1234', true, true);
-INSERT INTO public."user" VALUES (DEFAULT,'Roberto', '12345', true, false);
+ALTER TABLE ONLY public.beverage ALTER COLUMN id SET DEFAULT nextval('public.beverage_id_seq'::regclass);
 
 
 --
--- TOC entry 4639 (class 2606 OID 16405)
+-- TOC entry 4641 (class 2604 OID 16474)
+-- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
+
+
+--
+-- TOC entry 4643 (class 2606 OID 16476)
 -- Name: beverage beverage_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -108,7 +132,7 @@ ALTER TABLE ONLY public.beverage
 
 
 --
--- TOC entry 4641 (class 2606 OID 16410)
+-- TOC entry 4645 (class 2606 OID 16478)
 -- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -116,7 +140,7 @@ ALTER TABLE ONLY public."user"
     ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 
--- Completed on 2024-01-04 14:03:43
+-- Completed on 2024-01-05 19:29:04
 
 --
 -- PostgreSQL database dump complete
